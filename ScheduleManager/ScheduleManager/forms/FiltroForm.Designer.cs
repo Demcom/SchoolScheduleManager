a9bbcce43d3ebf2cac1da84f071e48011073da2b
Juan Camargo <juan.mc.diaz@gmail.com>
2017-07-03 20:16:28 -0700

    Cambios

--- ScheduleManager/ScheduleManager/forms/FiltroForm.Designer.cs
+++ ScheduleManager/ScheduleManager/forms/FiltroForm.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FiltroForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "FiltroForm";
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "FiltroForm";
+            this.Text = "Filtrar Información";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
